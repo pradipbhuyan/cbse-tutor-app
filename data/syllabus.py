3ee8766555e110fc3c7abf4ea12480cdec4f9943
pradipbhuyan <pradip.bhuyan@gmail.com>
2026-05-18 12:29:55 +0530

    Create syllabus.py

---
+++ data/syllabus.py
@@ -0,0 +1,71 @@
+CBSE_9 = {
+    "Science": [
+        "Matter in Our Surroundings",
+        "Is Matter Around Us Pure",
+        "Atoms and Molecules",
+        "Structure of the Atom",
+        "The Fundamental Unit of Life",
+        "Tissues",
+        "Motion",
+        "Force and Laws of Motion",
+        "Gravitation",
+        "Work and Energy",
+        "Sound",
+        "Improvement in Food Resources"
+    ],
+    "Maths": [
+        "Number Systems",
+        "Polynomials",
+        "Coordinate Geometry",
+        "Linear Equations in Two Variables",
+        "Introduction to Euclid's Geometry",
+        "Lines and Angles",
+        "Triangles",
+        "Quadrilaterals",
+        "Circles",
+        "Heron's Formula",
+        "Surface Areas and Volumes",
+        "Statistics"
+    ],
+    "English": [
+        "Reading Comprehension",
+        "Writing Skills",
+        "Grammar",
+        "Beehive Prose",
+        "Beehive Poetry",
+        "Moments Supplementary Reader"
+    ],
+    "Social Science": [
+        "History",
+        "Geography",
+        "Political Science",
+        "Economics"
+    ],
+    "Hindi": [
+        "क्षितिज / स्पर्श",
+        "कृतिका / संचयन",
+        "व्याकरण",
+        "लेखन कौशल",
+        "पठन कौशल"
+    ]
+}
+
+SOF_9 = {
+    "Science Olympiad": [
+        "Logical Reasoning",
+        "Science",
+        "Achievers Section"
+    ],
+    "Maths Olympiad": [
+        "Logical Reasoning",
+        "Mathematical Reasoning",
+        "Everyday Mathematics",
+        "Achievers Section"
+    ],
+    "English Olympiad": [
+        "Word and Structure Knowledge",
+        "Reading",
+        "Spoken and Written Expression",
+        "Achievers Section"
+    ]
+}
