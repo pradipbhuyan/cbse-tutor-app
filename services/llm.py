d846f95c2494e44a7eae01061e3fde07c3655a7d
pradipbhuyan <pradip.bhuyan@gmail.com>
2026-05-18 12:49:46 +0530

    Update llm.py

--- services/llm.py
+++ services/llm.py
@@ -1,5 +1,5 @@
 import os
-from openai import OpenAI, APIConnectionError, APIStatusError, AuthenticationError
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,33 +7,29 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def ask_llm(system_prompt: str, user_prompt: str) -> str:
-    try:
-        response = client.responses.create(
-            model="gpt-4.1-mini",
-            input=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt}
-            ],
-            temperature=0.4
-        )
-        return response.output_text
 
-    except AuthenticationError:
-        return "Authentication failed. Please check your OPENAI_API_KEY in the .env file."
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+        temperature=0.4
+    )
 
-    except APIConnectionError:
-        return """
-Connection error while contacting OpenAI API.
+    return response.output_text
 
-Please check:
-1. Internet connection
-2. VPN/proxy/firewall
-3. Whether https://api.openai.com is accessible
-4. Try using a mobile hotspot
-"""
 
-    except APIStatusError as e:
-        return f"OpenAI API returned an error: {e.status_code} - {e.response}"
+def generate_speech(text, output_file="lesson.mp3"):
 
-    except Exception as e:
-        return f"Unexpected error: {str(e)}"
+    speech_file_path = output_file
+
+    with client.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice="alloy",
+        input=text
+    ) as response:
+
+        response.stream_to_file(speech_file_path)
+
+    return speech_file_path
